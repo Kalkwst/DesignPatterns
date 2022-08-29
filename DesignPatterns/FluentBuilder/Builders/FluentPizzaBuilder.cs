20aa7e529b29a4911e586ea0f3a8eda69fd254c4
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2022-08-29 15:05:01 +0300

    :sparkles: Add Implementation for FluentBuilder

---
+++ DesignPatterns/FluentBuilder/Builders/FluentPizzaBuilder.cs
@@ -0,0 +1,56 @@
+﻿
+using FluentBuilder.Products;
+
+namespace FluentBuilder.Builders
+{
+    public class FluentPizzaBuilder
+    {
+        private readonly Pizza pizza;
+
+        public FluentPizzaBuilder(string pizzaName)
+        {
+            pizza = new Pizza(pizzaName);
+        }
+
+        public FluentPizzaBuilder WithDough(string dough)
+        {
+            pizza["dough"] = dough;
+            return this;
+        }
+
+        public FluentPizzaBuilder WithSauce(string sauce)
+        {
+            pizza["sauce"] = sauce;
+            return this;
+        }
+
+        public FluentPizzaBuilder WithMeat(string meat)
+        {
+            pizza["meats"] = meat;
+            return this;
+        }
+
+        public FluentPizzaBuilder WithCheese(string cheese)
+        {
+            pizza["cheeses"] = cheese;
+            return this;
+        }
+
+        public FluentPizzaBuilder WithVeggie(string veggie)
+        {
+            pizza["veggies"] = veggie;
+            return this;
+        }
+
+        public FluentPizzaBuilder WithExtra(string extra)
+        {
+            pizza["extras"] = extra;
+            return this;
+        }
+
+        public Pizza Build()
+        {
+            return pizza;
+        }
+    }
+}
---
+++ DesignPatterns/FluentBuilder/Program.cs
@@ -0,0 +1,14 @@
+﻿using FluentBuilder.Builders;
+
+var pizzaBuilder = new FluentPizzaBuilder("Supreme");
+
+var pizzaSupreme = pizzaBuilder
+   .WithDough("12-grain pizza dough")
+   .WithSauce("Tomato base")
+   .WithMeat("Pepperoni, Seasoned Minced Beef, Spicy Pork Sausage")
+   .WithVeggie("Mushroom, Mixed Peppers, Red Onions")
+   .WithCheese("Mozzarella")
+   .WithExtra("Jalapenos")
+   .Build();
+
+pizzaSupreme.Display();
